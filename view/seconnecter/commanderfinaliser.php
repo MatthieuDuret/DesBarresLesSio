1c3082454719a41b94915e55e0e51d57a480620a
MatthieuDuret <47315767+MatthieuDuret@users.noreply.github.com>
2019-11-11 20:18:22 +0100

    Add files via upload

---
+++ view/seconnecter/commanderfinaliser.php
@@ -0,0 +1,17 @@
+  <section class="page-section" id="commanderfinaliser">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12 text-center">
+			<h1>Merci pour votre achat !</h1>
+			<br> <br>
+			<h5> Paiement effectué ! </h5>
+
+<a href="<?= WEBROOT.'blague' ?>">
+	<button type="button" style="margin:20px" >
+		Continuer
+	</button>
+</a>
+</div>
+</div>
+</div>
+</section>
